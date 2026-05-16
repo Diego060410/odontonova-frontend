7a6fa3aeb30c86628e481d72eca4644a4e499de4
Diego060410 <diegoluis0604@gmail.com>
2026-05-16 06:12:39 -0500

    conexion backend render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://odontologobackend.onrender.com/api",
   timeout: 10000, // ⏱ evita requests colgadas
   headers: {
     "Content-Type": "application/json",
